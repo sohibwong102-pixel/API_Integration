a1c411e46d758d65d924e892f602b46776780fbb
Shobi <sohibwong102@gmail.com>
2026-05-21 15:56:49 +0700

    update goblin 😎🔥

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,25 +2,22 @@
 
 MESSAGE=${1:-"update goblin 😎🔥"}
 
-if gum spin --spinner monkey --title "😎 adding files..." -- sleep 3 ; then git add -A
+if gum spin --spinner monkey --title "😎 adding files..." -- sh -c "sleep 2 && git add -A"; then
     echo "✅ add success"
-    sleep 1
 else
     echo "❌ add failed"
     exit 1
 fi
 
-if gum spin --spinner monkey --title "😎 committing..." -- git commit -m "$MESSAGE"; then
+if gum spin --spinner monkey --title "😎 committing..." -- sh -c "sleep 2 && git commit -m '$MESSAGE'"; then
     echo "✅ commit success"
-    sleep 1
 else
     echo "❌ commit failed"
     exit 1
 fi
 
-if gum spin --spinner monkey --title "😎 pushing..." -- git push; then
+if gum spin --spinner monkey --title "😎 pushing..." -- sh -c "sleep 2 && git push"; then
     echo "✅ push success"
-    sleep 3
 else
     echo "❌ push failed"
     exit 1
